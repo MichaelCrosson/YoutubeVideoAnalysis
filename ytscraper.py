eef8bf28fd53b062f78c53fe69b8135f5e9df12b
mikalalo <mikala.lowrance@gmail.com>
2024-11-15 10:15:54 -0600

    updated video scraper
    now loops over urls

--- ytscraper.py
+++ ytscraper.py
@@ -1,16 +1,22 @@
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
+import pandas as pd
 
-url = "https://www.youtube.com/watch?v=Lpk--3LH9Ck"
+videos_df = pd.read_csv('videos_df.csv')
+urls = videos_df['video_id']
 
-# Specify your desired download path
-download_path = "C:/Users/nosso/OneDrive/Desktop" 
+for url in urls: 
+    try:
+        # Specify your desired download path
+        download_path = "Videos" 
 
-yt = YouTube(url, on_progress_callback=on_progress)
-print(yt.title)
+        yt = YouTube(url, on_progress_callback=on_progress)
+        print(yt.title)
 
-# Get the highest resolution stream
-ys = yt.streams.get_highest_resolution()
+        # Get the highest resolution stream
+        ys = yt.streams.get_highest_resolution()
 
-# Download the video to the specified path
-ys.download(output_path=download_path)
+        # Download the video to the specified path
+        ys.download(output_path=download_path)
+    except:
+        print(f"Failed to download {url}: {e}")
